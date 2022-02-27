7eb0957788902739603cd597f770a779b3eb778b
Ben George Netto <bengnetto245@gmail.com>
2022-02-27 20:55:28 +0530

    edit: CharFrequency - added user input

--- src/CharFrequency.java
+++ src/CharFrequency.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class CharFrequency {
     public static void main(String[] args) {
-        String str = "This is a sample string";
-        char ch = 'i';
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println("\nEnter character to check frequency: ");
+        char ch = sc.next().charAt(0);
+
         int f = 0;
 
+        String strL = str.toLowerCase();
+        char chL = Character.toLowerCase(ch);
+
         for(int i = 0; i < str.length(); ++i) {
-            if(ch == str.charAt(i)) {
+            if(chL == strL.charAt(i)) {
                 ++f;
             }
         }
 
-        System.out.println("Frequency of " + ch + " in string = " + f);
+        System.out.println("Frequency of " + ch + " in given string = " + f);
+
+        sc.close();
     }
 }
